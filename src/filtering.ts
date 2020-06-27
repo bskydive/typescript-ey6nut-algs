21d26aed2eb30be8fbd62be1f36a9fdbdd766042
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2020-06-27 16:06:48 +0300

    iniy

---
+++ src/filtering.ts
@@ -0,0 +1,53 @@
+
+import { logAll, IRunListItem } from './utils';
+import { interval, of } from 'rxjs';
+import { take, map, endWith} from 'rxjs/operators';
+
+/**
+ * Операторы фильтрации
+ * 
+ * для массового выполнения тестов, комментировать не надо, запуск управляется из index.ts
+ * filteringOperatorList.push({ observable$: xxx$ });
+ * 
+ * раскомментировать для ручного запуска
+ * xxx$.subscribe((item) => logAll('получил: ', item), err => logAll('ошибка:', err), () => logAll('skip поток закрыт'));
+ */
+export const filteringOperatorList: IRunListItem[] = [];
+
+//========================================================================================================================
+//==================================================FILTERING ONE=========================================================
+//========================================================================================================================
+//указанные операторы получают и возвращают значения в потоке
+
+/**
+ * skip
+ * скрывает указанное количество значений
+
+
+получил:  0-1
+получил:  101-1
+получил:  202-1
+получил:  303-1
+получил:  306-2
+получил:  404-1
+получил:  101-закрыт
+получил:  408-2
+получил:  102-закрыт
+skip поток закрыт
+ */
+
+const skipSrc1$ = interval(101).pipe(take(5), map(item => item * 101 + '-1'), endWith('101-закрыт'));
+const skipSrc2$ = interval(102).pipe(
+	take(5),
+	map(item => item * 102 + '-2'),
+	skip(3), endWith('102-закрыт')
+);
+
+const skip$ = of(skipSrc1$, skipSrc2$).pipe(
+	mergeAll()
+)
+
+//skip$.subscribe((item) => logAll('получил: ', item), err => logAll('ошибка:', err), () => logAll('skip поток закрыт'));
+filteringOperatorList.push({ observable$: skip$ });
+
+
---
+++ src/utils.ts
@@ -0,0 +1,30 @@
+import { Observable } from 'rxjs';
+
+/**
+ * Интерфейсы и служебные функции
+ * 
+ */
+
+/**
+ * Чтобы обойти ошибку TS2496: The 'arguments' object cannot be referenced in an arrow function in ES3 and ES5. Consider using a standard function expression.
+ * https://github.com/microsoft/TypeScript/issues/1609
+ * Чтобы не светились ошибки использования console.log
+ * Здесь такое логирование применимо, на проде - нет
+ */
+export function logAll(...values) {
+	console.log(...values); // ...arguments
+}
+
+export type TTypeItem = 'observable' | 'observable' | 'primitive' | 'function' | 'object';
+export type TOperationItem = 'filter' | 'sort' | 'parse' | 'reply' | 'exhaust' | 'merge' | 'last' | 'debounce' | 'time' | 'accumulate' | 'recursion' | 'switch' | 'array/concat' | 'flattening' | 'missing' | 'unsubscribing';
+
+/**
+ * Интерфейс для построения модулей, поиска и тестирования операторов
+ */
+export interface IRunListItem {
+	observable$: Observable<any>;
+	inputTypeList?: TTypeItem[];
+	outputTypeList?: TTypeItem[];
+	operationTypeList?: TOperationItem[];
+	result?: any;
+}
